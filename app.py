24811a792c6b158df9301b338f3cb3d4dfcc0734
ManishMeena788 <119215519+ManishMeena788@users.noreply.github.com>
2022-12-24 15:38:23 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,25 +1,54 @@
 from flask import Flask
 from pyrogram import Client, filters, idle
 from main import Bot
+import threading
+
+
 app = Flask(__name__)
- 
+
+#pyro = Client("account")
+
+pyro = Client(
+    "LibraryBot",
+    bot_token="5976715403:AAGgEOL7PmixhQWfa7p3avxsGF9biQgtl90",
+    api_id=1774230,
+    api_hash="b0829cf5b62052d6c8adee27b02f1f00"
+    )
+
+@app.route("/")
+def hello_worldyy():
+    return str(pyro.get_me())
+
 @app.route('/home')
 def hello_world():
   return 'GreyMatter_Botskk'
 
-@app.route('/')
-def hello_worldy():
-  print("akhil")
-  Bot.start()
-  idle()
-  Bot.stop()
+pyro.start()
+threading.Thread(target=app.run, daemon=True).start()
+idle()
+pyro.stop()
+
+#@app.route('/')
+#def hello_worldy():
+#  print("akhil")
+#  Bot.start()
+#  idle()
+#  Bot.stop()
 
 #def Pyroggram():
   
-print("kyuki kyaa hotaa h")
+#print("kyuki kyaa hotaa h")
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #print("akhil2")
     #Pyroggram()
-    app.run(debug="True")
+#    app.run(debug="True")
+
+
+
+
+
+
+
+
