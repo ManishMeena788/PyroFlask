4ec20df309f7cdcb1ef6e121993309d48516c463
ManishMeena788 <119215519+ManishMeena788@users.noreply.github.com>
2022-12-24 15:10:26 +0530

    update app.py

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from pyrogram import Client, filters, idle
 from main import Bot
 app = Flask(__name__)
  
-@app.route('/')
+@app.route('/home')
 def hello_world():
   return 'GreyMatter_Botskk'
+
+@app.route('/')
+def hello_worldy():
   print("akhil")
   Bot.start()
   idle()
